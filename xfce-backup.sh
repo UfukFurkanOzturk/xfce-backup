bc332ebea0ceb291b86d8c01905bf51029ed148a
UfukFurkanOzturk <ufukfurkanozturk@gmail.com>
2023-06-11 01:59:54 +0300

    update 0.0.2

--- xfce-backup.sh
+++ xfce-backup.sh
@@ -4,10 +4,54 @@
 # for restore from backup use "./xfce-backup.sh restore"
 # while using restore, xfce4-backup.tar.gz have to be in the same directory with this script
 MODE=$1 # mode
-VERSION="0.0.1"
+VERSION="0.0.2"
+
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if exists gsettings; then
+    :
+else
+  echo 'Cannot detect gsettings'
+  echo "if you're on Debian you can install with libglib2.0-bin package from official repositories"
+  exit
+fi
+
+if exists xfconf-query; then
+    :
+else
+    echo 'Cannot detect xfconf-query'
+    exit
+fi
+
+if exists tar; then
+    :
+else
+    echo 'Cannot detect tar'
+    exit
+fi
+
+if exists gzip; then
+    :
+else
+    echo 'Cannot detect gzip'
+fi
+
+if [ "$(id -u)" == 0 ]; then
+    echo 'you must use this script as home user'
+    exit
+else
+    :
+fi
+
+if [ -z "$1" ]; then
+    echo 'Please read how to use.'
+    exit
+fi
 
 # main backup function
-function backupmain() {
+backupmain() {
     #current themes
     eval THEME="$(gsettings get org.gnome.desktop.interface gtk-theme)"
     eval ICON="$(gsettings get org.gnome.desktop.interface icon-theme)"
@@ -17,17 +61,16 @@ function backupmain() {
     mkdir Theme && cp -r "/usr/share/themes/$THEME" ./Theme
     mkdir Icons && cp -r "/usr/share/icons/$ICON" ./Icons
     mkdir Cursor && cp -r "/usr/share/icons/$CURSOR" ./Cursor
-    cp "/$HOME/.face" .
     echo "$THEME" >> ./Theme/currenttheme
     echo "$ICON" >> ./Icons/currenticon
     echo "$CURSOR" >> ./Cursor/currentcursor && echo "$CURSORSIZE" >> ./Cursor/currentsize
     echo "$VERSION" >> version
-    tar -czf ./xfce4-backup.tar.gz xfce4 Theme Icons Cursor .face version
+    tar -czf ./xfce4-backup.tar.gz xfce4 Theme Icons Cursor version
     cp ./xfce4-backup.tar.gz "./out/"
-    rm -r xfce4 Theme Icons Cursor .face xfce4-backup.tar.gz version
+    rm -r xfce4 Theme Icons Cursor xfce4-backup.tar.gz version
 }
 
-function backup() {
+backup() {
     if [ -f "./out/xfce4-backup.tar.gz" ]; then
         backupmain
         echo "backup file successfully overwritten!"
@@ -39,14 +82,13 @@ function backup() {
     fi
 }
 
-function restore() {
+restore() {
     tar -xf ./xfce4-backup.tar.gz
     THEME=$(cat ./Theme/currenttheme)
     ICON=$(cat ./Icons/currenticon)
     CURSOR=$(cat ./Cursor/currentcursor)
     CURSORSIZE=$(cat ./Cursor/currentsize)
     cp -r xfce4 "$HOME/.config/"
-    cp .face "/$HOME/"
     sudo cp -r "./Theme/$THEME" "/usr/share/themes/"
     sudo cp -r "./Icons/$ICON" "/usr/share/icons/"
     sudo cp -r "./Cursor/$CURSOR" "/usr/share/icons/"
@@ -54,34 +96,29 @@ function restore() {
     xfconf-query -c xsettings -p /Net/IconThemeName -s "$ICON"
     xfconf-query -c xsettings -p /Gtk/CursorThemeName -s "$CURSOR"
     xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s "$CURSORSIZE"
-    rm -r xfce4 Theme Icons Cursor .face
+    rm -r xfce4 Theme Icons Cursor
     echo "xfce config and theme restored"
 }
 
-# check if the file ran as root
-if [[ $(id -u) != 0 ]]; then
-    # Check for the mode
-    if [ "$MODE" = backup ]; then
-        if [ -d "/$HOME/.config/xfce4/" ]; then
-            # create an output file if it isn't exists
-            if [ -d "./out/" ]; then
-                :
-            else
-                mkdir out
-            fi
-            backup
+# Check for the mode
+if [ "$MODE" = backup ]; then
+    if [ -d "/$HOME/.config/xfce4/" ]; then
+        # create an output file if it isn't exists
+        if [ -d "./out/" ]; then
+            :
         else
-            echo "couldn't find the config"
+            mkdir out
         fi
-    elif [ "$MODE" = restore ]; then
-        if [ -f "./xfce4-backup.tar.gz" ]; then
-            restore
-        else
-            echo "couldn't find the config"
-        fi
+        backup
     else
-        echo "error '$MODE' is not an argument use 'backup' or 'restore'"
+        echo "couldn't find the config"
+    fi
+elif [ "$MODE" = restore ]; then
+    if [ -f "./xfce4-backup.tar.gz" ]; then
+        restore
+    else
+        echo "couldn't find the config"
     fi
 else
-    echo "don't run this as root"
+    echo "error '$MODE' is not an argument use 'backup' or 'restore'"
 fi
